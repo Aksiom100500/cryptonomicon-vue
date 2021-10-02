9af7389323f492f61ace7a00533c4c093fac9b49
aksiom <aksiom100500@gmail.com>
2021-10-02 13:30:09 +0300

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,11 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-    <div
-      :class="{
-        hidden: coins,
-      }"
-      class="
-        fixed
-        w-100
-        h-100
-        opacity-80
-        bg-purple-800
-        inset-0
-        z-50
-        flex
-        items-center
-        justify-center
-      "
-    >
-      <svg
-        class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    </div>
+    <!-- <div class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
+    <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+  </div> -->
     <div class="container">
       <section>
         <div class="flex">
@@ -297,14 +265,7 @@ export default {
       tickers: [{ name: "BTC", price: "-" }],
       selected: null,
       graph: [],
-      coins: null,
     };
-  },
-  async created() {
-    const f = await fetch(
-      "https://min-api.cryptocompare.com/data/blockchain/list?api_key=46713c785b9eafe108d9fa4ee5ca0e81ba1da74852ffc152b3bda86276e51d25"
-    );
-    this.coins = await f.json();
   },
   methods: {
     add() {
